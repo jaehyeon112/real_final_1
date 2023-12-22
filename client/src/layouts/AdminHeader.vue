797b85c0fd4e37730d74db4b7b30dd852ee3bb9c
younghee <hn05035@naver.com>
2023-12-22 17:47:31 +0900

    안냥

--- client/src/layouts/AdminHeader.vue
+++ client/src/layouts/AdminHeader.vue
@@ -8,37 +8,7 @@
           <router-link class="nav-link" to="/logout">로그아웃</router-link>
         </li>
       </ul>
-  
-      <div class="container text-center">
-        <div class="row">
-          <div class="col">
-            <router-link
-              to="/"
-              class="d-flex align-items-center mb-3 mb-lg-0 me-lg-auto link-body-emphasis text-decoration-none"
-            >
-              <!--이미지 찾아서 넣자-->
-              <svg class="bi me-2" width="40" height="32">
-                <use xlink:href="#bootstrap"></use>
-              </svg>
-              <span class="fs-4">기업 로고</span>
-            </router-link>
-          </div>
-          <div class="col-6">
-            <form class="col-12 col-lg-6 mb-3 mb-lg-0" role="search">
-              <input type="search" placeholder="Search..." />
-            </form>
-          </div>
-          <div class="col-2">
-            <router-link to="/">
-              <i class="bi bi-bell"></i>
-            </router-link>
-            <span style="color: #fff">　</span>
-            <router-link to="/">
-              <i class="bi bi-cart"> </i>
-            </router-link>
-          </div>
-        </div>
-      </div>
+
       <div
         class="text-center"
         id="lnb"
@@ -55,6 +25,7 @@
             padding-bottom: 10px;
           "
         >
+
           <div class="col">
             <router-link class="nav-custom" to="">마이페이지</router-link>
           </div>
@@ -76,6 +47,7 @@
         </div>
       </div>
     </div>
+    
   </template>
   
   <script>
--- client/src/layouts/SideBar.vue
+++ client/src/layouts/SideBar.vue
@@ -1,120 +1,238 @@
 <template>
+          <!-- ======= Sidebar ======= -->
+          <aside>
 
-<aside id="layout-menu" class="layout-menu menu-vertical menu bg-menu-theme">
+<ul class="sidebar-nav" id="sidebar-nav">
 
-    <div class="menu-inner-shadow"></div>
+  <li class="nav-item">
+    <a class="nav-link collapsed" href="index.html">
+      <i class="bi bi-grid"></i>
+      <span>Dashboard</span>
+    </a>
+  </li><!-- End Dashboard Nav -->
 
-    <ul class="menu-inner py-1">
-      <!-- Dashboard -->
-      <li class="menu-item active">
-        <a href="index.html" class="menu-link">
-          <i class="menu-icon tf-icons bx bx-home-circle"></i>
-          <div data-i18n="Analytics">마이페이지</div>
+  <li class="nav-item">
+    <a class="nav-link collapsed" data-bs-target="#components-nav" data-bs-toggle="collapse" href="#">
+      <i class="bi bi-menu-button-wide"></i><span>Components</span><i class="bi bi-chevron-down ms-auto"></i>
+    </a>
+    <ul id="components-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+      <li>
+        <a href="components-alerts.html">
+          <i class="bi bi-circle"></i><span>Alerts</span>
         </a>
       </li>
-
-      <!-- Layouts -->
-      <li class="menu-item">
-        <a href="javascript:void(0);" class="menu-link menu-toggle">
-          <i class="menu-icon tf-icons bx bx-layout"></i>
-          <div data-i18n="Layouts">상품관리</div>
+      <li>
+        <a href="components-accordion.html">
+          <i class="bi bi-circle"></i><span>Accordion</span>
         </a>
-
-        <ul class="menu-sub">
-          <li class="menu-item">
-            <a href="layouts-without-menu.html" class="menu-link">
-              <div data-i18n="Without menu">상품리스트</div>
-            </a>
-          </li>
-          <li class="menu-item">
-            <a href="layouts-without-navbar.html" class="menu-link">
-              <div data-i18n="Without navbar">상품등록</div>
-            </a>
-          </li>
-        </ul>
       </li>
-
-      <li class="menu-header small text-uppercase">
-        <a href="javascript:void(0);" class="menu-link menu-toggle">
-          <i class="menu-icon tf-icons bx bx-layout"></i>
-          <div data-i18n="Layouts">회원관리</div>
+      <li>
+        <a href="components-badges.html">
+          <i class="bi bi-circle"></i><span>Badges</span>
         </a>
-        <ul class="menu-sub">
-          <li class="menu-item">
-            <a href="layouts-without-menu.html" class="menu-link">
-              <div data-i18n="Without menu">회원리스트</div>
-            </a>
-          </li>
-        </ul>
       </li>
-      
-      
-      <li class="menu-item">
-        <a href="javascript:void(0);" class="menu-link menu-toggle">
-          <i class="menu-icon tf-icons bx bx-dock-top"></i>
-          <div data-i18n="Account Settings">리뷰/문의사항</div>
+      <li>
+        <a href="components-breadcrumbs.html">
+          <i class="bi bi-circle"></i><span>Breadcrumbs</span>
         </a>
-        <ul class="menu-sub">
-          <li class="menu-item">
-            <a href="pages-account-settings-account.html" class="menu-link">
-              <div data-i18n="Account">Account</div>
-            </a>
-          </li>
-          <li class="menu-item">
-            <a href="pages-account-settings-notifications.html" class="menu-link">
-              <div data-i18n="Notifications">Notifications</div>
-            </a>
-          </li>
-          <li class="menu-item">
-            <a href="pages-account-settings-connections.html" class="menu-link">
-              <div data-i18n="Connections">Connections</div>
-            </a>
-          </li>
-        </ul>
       </li>
-      <li class="menu-item">
-        <a href="javascript:void(0);" class="menu-link menu-toggle">
-          <i class="menu-icon tf-icons bx bx-lock-open-alt"></i>
-          <div data-i18n="Authentications">주문관리</div>
+      <li>
+        <a href="components-buttons.html">
+          <i class="bi bi-circle"></i><span>Buttons</span>
         </a>
-        <ul class="menu-sub">
-          <li class="menu-item">
-            <a href="auth-login-basic.html" class="menu-link" target="_blank">
-              <div data-i18n="Basic">Login</div>
-            </a>
-          </li>
-          <li class="menu-item">
-            <a href="auth-register-basic.html" class="menu-link" target="_blank">
-              <div data-i18n="Basic">Register</div>
-            </a>
-          </li>
-          <li class="menu-item">
-            <a href="auth-forgot-password-basic.html" class="menu-link" target="_blank">
-              <div data-i18n="Basic">Forgot Password</div>
-            </a>
-          </li>
-        </ul>
       </li>
-      <li class="menu-item">
-        <a href="javascript:void(0);" class="menu-link menu-toggle">
-          <i class="menu-icon tf-icons bx bx-cube-alt"></i>
-          <div data-i18n="Misc">통계</div>
+      <li>
+        <a href="components-cards.html">
+          <i class="bi bi-circle"></i><span>Cards</span>
         </a>
-        <ul class="menu-sub">
-          <li class="menu-item">
-            <a href="pages-misc-error.html" class="menu-link">
-              <div data-i18n="Error">Error</div>
-            </a>
-          </li>
-          <li class="menu-item">
-            <a href="pages-misc-under-maintenance.html" class="menu-link">
-              <div data-i18n="Under Maintenance">Under Maintenance</div>
-            </a>
-          </li>
-        </ul>
       </li>
-
+      <li>
+        <a href="components-carousel.html">
+          <i class="bi bi-circle"></i><span>Carousel</span>
+        </a>
+      </li>
+      <li>
+        <a href="components-list-group.html">
+          <i class="bi bi-circle"></i><span>List group</span>
+        </a>
+      </li>
+      <li>
+        <a href="components-modal.html">
+          <i class="bi bi-circle"></i><span>Modal</span>
+        </a>
+      </li>
+      <li>
+        <a href="components-tabs.html">
+          <i class="bi bi-circle"></i><span>Tabs</span>
+        </a>
+      </li>
+      <li>
+        <a href="components-pagination.html">
+          <i class="bi bi-circle"></i><span>Pagination</span>
+        </a>
+      </li>
+      <li>
+        <a href="components-progress.html">
+          <i class="bi bi-circle"></i><span>Progress</span>
+        </a>
+      </li>
+      <li>
+        <a href="components-spinners.html">
+          <i class="bi bi-circle"></i><span>Spinners</span>
+        </a>
+      </li>
+      <li>
+        <a href="components-tooltips.html">
+          <i class="bi bi-circle"></i><span>Tooltips</span>
+        </a>
+      </li>
     </ul>
-  </aside>
-      
+  </li><!-- End Components Nav -->
+
+  <li class="nav-item">
+    <a class="nav-link " data-bs-target="#forms-nav" data-bs-toggle="collapse" href="#">
+      <i class="bi bi-journal-text"></i><span>Forms</span><i class="bi bi-chevron-down ms-auto"></i>
+    </a>
+    <ul id="forms-nav" class="nav-content collapse show" data-bs-parent="#sidebar-nav">
+      <li>
+        <a href="forms-elements.html">
+          <i class="bi bi-circle"></i><span>Form Elements</span>
+        </a>
+      </li>
+      <li>
+        <a href="forms-layouts.html" class="active">
+          <i class="bi bi-circle"></i><span>Form Layouts</span>
+        </a>
+      </li>
+      <li>
+        <a href="forms-editors.html">
+          <i class="bi bi-circle"></i><span>Form Editors</span>
+        </a>
+      </li>
+      <li>
+        <a href="forms-validation.html">
+          <i class="bi bi-circle"></i><span>Form Validation</span>
+        </a>
+      </li>
+    </ul>
+  </li><!-- End Forms Nav -->
+
+  <li class="nav-item">
+    <a class="nav-link collapsed" data-bs-target="#tables-nav" data-bs-toggle="collapse" href="#">
+      <i class="bi bi-layout-text-window-reverse"></i><span>Tables</span><i class="bi bi-chevron-down ms-auto"></i>
+    </a>
+    <ul id="tables-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+      <li>
+        <a href="tables-general.html">
+          <i class="bi bi-circle"></i><span>General Tables</span>
+        </a>
+      </li>
+      <li>
+        <a href="tables-data.html">
+          <i class="bi bi-circle"></i><span>Data Tables</span>
+        </a>
+      </li>
+    </ul>
+  </li><!-- End Tables Nav -->
+
+  <li class="nav-item">
+    <a class="nav-link collapsed" data-bs-target="#charts-nav" data-bs-toggle="collapse" href="#">
+      <i class="bi bi-bar-chart"></i><span>Charts</span><i class="bi bi-chevron-down ms-auto"></i>
+    </a>
+    <ul id="charts-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+      <li>
+        <a href="charts-chartjs.html">
+          <i class="bi bi-circle"></i><span>Chart.js</span>
+        </a>
+      </li>
+      <li>
+        <a href="charts-apexcharts.html">
+          <i class="bi bi-circle"></i><span>ApexCharts</span>
+        </a>
+      </li>
+      <li>
+        <a href="charts-echarts.html">
+          <i class="bi bi-circle"></i><span>ECharts</span>
+        </a>
+      </li>
+    </ul>
+  </li><!-- End Charts Nav -->
+
+  <li class="nav-item">
+    <a class="nav-link collapsed" data-bs-target="#icons-nav" data-bs-toggle="collapse" href="#">
+      <i class="bi bi-gem"></i><span>Icons</span><i class="bi bi-chevron-down ms-auto"></i>
+    </a>
+    <ul id="icons-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+      <li>
+        <a href="icons-bootstrap.html">
+          <i class="bi bi-circle"></i><span>Bootstrap Icons</span>
+        </a>
+      </li>
+      <li>
+        <a href="icons-remix.html">
+          <i class="bi bi-circle"></i><span>Remix Icons</span>
+        </a>
+      </li>
+      <li>
+        <a href="icons-boxicons.html">
+          <i class="bi bi-circle"></i><span>Boxicons</span>
+        </a>
+      </li>
+    </ul>
+  </li><!-- End Icons Nav -->
+
+  <li class="nav-heading">Pages</li>
+
+  <li class="nav-item">
+    <a class="nav-link collapsed" href="users-profile.html">
+      <i class="bi bi-person"></i>
+      <span>Profile</span>
+    </a>
+  </li><!-- End Profile Page Nav -->
+
+  <li class="nav-item">
+    <a class="nav-link collapsed" href="pages-faq.html">
+      <i class="bi bi-question-circle"></i>
+      <span>F.A.Q</span>
+    </a>
+  </li><!-- End F.A.Q Page Nav -->
+
+  <li class="nav-item">
+    <a class="nav-link collapsed" href="pages-contact.html">
+      <i class="bi bi-envelope"></i>
+      <span>Contact</span>
+    </a>
+  </li><!-- End Contact Page Nav -->
+
+  <li class="nav-item">
+    <a class="nav-link collapsed" href="pages-register.html">
+      <i class="bi bi-card-list"></i>
+      <span>Register</span>
+    </a>
+  </li><!-- End Register Page Nav -->
+
+  <li class="nav-item">
+    <a class="nav-link collapsed" href="pages-login.html">
+      <i class="bi bi-box-arrow-in-right"></i>
+      <span>Login</span>
+    </a>
+  </li><!-- End Login Page Nav -->
+
+  <li class="nav-item">
+    <a class="nav-link collapsed" href="pages-error-404.html">
+      <i class="bi bi-dash-circle"></i>
+      <span>Error 404</span>
+    </a>
+  </li><!-- End Error 404 Page Nav -->
+
+  <li class="nav-item">
+    <a class="nav-link collapsed" href="pages-blank.html">
+      <i class="bi bi-file-earmark"></i>
+      <span>Blank</span>
+    </a>
+  </li><!-- End Blank Page Nav -->
+
+</ul>
+
+</aside><!-- End Sidebar-->
 </template>
