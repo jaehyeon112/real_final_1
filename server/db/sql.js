205f6afd0a751061eb3ad71a45b97f7cc32da44e
yeb1012 <tgu02007@naver.com>
2023-12-26 00:29:37 +0900

    bbbbb

--- server/db/sql.js
+++ server/db/sql.js
@@ -3,10 +3,18 @@ let test = {
 };
 let reviews = {
   myList:`select * from review where user_id=? `,//마이페이지에서 내가 작성한 리뷰 리스트
-  detailList:`select * from review where prod_no=?` //상세페이지에서 그 상품에대한 리뷰 리스트
-  
-}
+  detailList:`select * from review where prod_no=?`, //상세페이지에서 그 상품에대한 리뷰 리스트
+  insertRivew:`insert into review set?`,//이미지를 넣으려면..?
+  insertImage:`insert into image set?`  
+};
+let point = {
+  myPoint:`select * from point where user_id=?`//마이페이지 보유 포인트
+};
+let coupon = {
+  myCoupon:`select * from coupon where user_id=?`//마이페이지 보유 쿠폰
+};
+
 
 module.exports = {
-  test,reviews
+  test,reviews,point,coupon
 };
