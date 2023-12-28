3ae451790efcd3c2d9185bfaf117e4e53acd2cd3
kjh <kjh@naver.com>
2023-12-29 00:25:21 +0900

    err

--- client/src/components/menu/MenuList.vue
+++ client/src/components/menu/MenuList.vue
@@ -2,24 +2,30 @@
   <v-container>
     <span>{{ prodList.prod_no }}구분!!</span>
     <v-card class="mx-auto" max-width="300">
-      <!--해당 제품으로 이동-->
+      <!-- 해당 제품으로 이동 -->
       <router-link to="/">
-        <v-img class="align-end text-white" height="300" src="/api/test" cover :class="{ active: isActive, 'text-danger': hasError}">
-          <v-row justify="end" style="margin-right: 30px; margin-bottom: 30px">
-            <v-btn icon="mdi mdi-cart" variant="tonal"> </v-btn>
-          </v-row>
-        </v-img>
+        <div class="image-container">
+          <v-img
+            class="align-end text-white"
+            height="300"
+            src="/api/test"
+            @load="imageLoaded"
+          >
+            <v-row justify="end" style="margin-right: 30px; margin-bottom: 30px">
+              <v-btn icon="mdi mdi-cart" variant="tonal" @click.prevent="goToCart"> </v-btn>
+            </v-row>
+          </v-img>
+          <div v-if="isSoldOut" class="soldout-overlay">상품 준비 중</div>
+        </div>
       </router-link>
 
       <v-card-subtitle class="pt-4"> 1조꺼 </v-card-subtitle>
 
       <v-card-text>
         <div>{{ prodList.prod_name }}</div>
-        <!-- 상품 부가 정보-->
+        <!-- 상품 부가 정보 -->
         <div>
-
           <span>{{ prodList.discount_rate }}%</span>
-      
           <span>{{ prodList.discount_price }}원(판매가)</span
           ><span>{{ prodList.price }}원(원가)</span>
         </div>
@@ -36,11 +42,22 @@
 export default {
   data() {
     return {
-      isActive:false
-    }
+      isSoldOut: false,
+    };
   },
   props: ["prodList"],
-  created() {
+  methods: {
+    soldout() {
+      if (this.prodList.soldout == 1) {
+        this.isSoldOut = true;
+      }
+    },
+    imageLoaded() {
+      this.soldout();
+    },
+    goToCart(){
+      alert("장바구니 만들고 난 뒤에~")
+    }
   },
 };
 </script>
@@ -53,5 +70,23 @@ export default {
   font-size: 12px;
   width: 100px;
   text-align: center;
+}
+
+.image-container {
+  position: relative;
+  text-align: center;
+  line-height: 300px;
+  color : red;
+  font-size:24px;
+}
+
+.soldout-overlay {
+  background-color: rgba(0, 0, 0, 0.5);
+  z-index: 2;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  position: absolute;
 }
 </style>
